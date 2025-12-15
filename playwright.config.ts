3ca7e59ddbfaeb84cbcf6060daf317370555071a
theshenron <gourav143faz@gmail.com>
2025-12-16 00:55:40 +0530

    playwright conf code updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['json', { outputFile: 'results.json' }]],
+  // reporter: [['json', { outputFile: 'results.json' }]],
+  reporter: [
+    ['json', { outputFile: 'playwright-report/report.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
   use: {
